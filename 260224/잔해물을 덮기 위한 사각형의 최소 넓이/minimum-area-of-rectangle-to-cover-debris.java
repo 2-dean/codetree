90d17645e1f38d9ff191a30b6cccdd3f5ef4b95b
2dean <104578954+2-dean@users.noreply.github.com>
2026-02-24 22:12:58 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 141ms, Memory: 26MB, Status: Runtime Error - Codetree

---
+++ 260224/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] paper = new int [2001][2001];
+        int rect1_x1 = sc.nextInt()+100;
+        int rect1_y1 = sc.nextInt()+100;
+        int rect1_x2 = sc.nextInt()+100;
+        int rect1_y2 = sc.nextInt()+100;
+
+        int rect2_x1 = sc.nextInt()+100;
+        int rect2_y1 = sc.nextInt()+100;
+        int rect2_x2 = sc.nextInt()+100;
+        int rect2_y2 = sc.nextInt()+100;
+        // Please write your code here.
+
+        for (int i = rect1_x1; i < rect1_x2; i ++ ) {
+            for (int j = rect1_y1; j < rect1_y2; j ++ ) {
+                paper[i][j] = 1;
+            }
+        }
+
+        for (int i = rect2_x1; i < rect2_x2; i ++ ) {
+            for (int j = rect2_y1; j < rect2_y2; j ++ ) {
+                paper[i][j] = 0;
+            }
+        }
+
+        // 가장 왼쪽/ 가장오른쪽 끝점 찾기
+        int minX = 201;
+        int maxX = 0;
+        int minY = 201;
+        int maxY = 0;
+        boolean found = false;
+
+        for (int i = 0; i < 201; i++) {
+            for (int j = 0; j < 201; j++) {
+                if (paper[i][j] == 1) {
+                    found = true; // 이걸 꼭 해줘야 0 출력이 안 돼요!
+                    if (i < minX) minX = i;
+                    if (i > maxX) maxX = i;
+                    if (j < minY) minY = j;
+                    if (j > maxY) maxY = j;  
+                }
+            }
+        }
+
+        if (!found) {
+            System.out.print(0);
+        } else {
+            int width = maxX - minX +1; //칸 
+            int height = maxY - minY + 1;
+            System.out.print(width * height);
+
+        }
+
+        
+    }
+}
