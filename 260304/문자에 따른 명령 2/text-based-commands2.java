d81404a048a085bea6a87528e46b9fe0bcd10e58
2dean <104578954+2-dean@users.noreply.github.com>
2026-03-04 18:36:27 +0900

    Title: 문자에 따른 명령 2, Time: 166ms, Memory: 11MB, Status: Runtime Error - Codetree

---
+++ 260304/문자에 따른 명령 2/text-based-commands2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[] dx = new int[]{1, 0, -1, 0};
+    static int[] dy = new int[]{0, -1, 0, 1};
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        // Please write your code here.
+        int dirNum = 3; // 북쪽
+        int nx = 0;        
+        int ny = 0;
+
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c =='L') {
+                dirNum = (dirNum + 4 - 1) % 4; // 
+            } else if (c =='R') { // 방향 전환
+                dirNum = (dirNum - 1) % 4;
+            } else if (c == 'F') {// 해당 방향으로 -1 이동
+                nx = x + dx[dirNum];
+                ny = y + dy[dirNum];
+            }
+        }
+       
+
+        System.out.print(nx +  " " + ny);
+
+    }
+}
