e8434aa4fd6448ae4c34735bd12e734a0025dbe2
2dean <104578954+2-dean@users.noreply.github.com>
2026-03-06 13:39:27 +0900

    Title: 되돌아오기, Time: 130ms, Memory: 14MB, Status: Wrong Answer - Codetree

--- 260306/되돌아오기/come-back.java
+++ 260306/되돌아오기/come-back.java
@@ -36,7 +36,7 @@ public class Main {
                 int ny = y + dy[dirNum];
                 time ++;
                 //처음자리
-                if (arr[nx][ny] == 1 ){
+                if (arr[nx][ny] != 0 ){
                     System.out.print(time);
                     return;
                 } 
