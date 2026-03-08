bd4f4eabed559ad000f6b3d9b1b11ab3280fa2ed
2dean <104578954+2-dean@users.noreply.github.com>
2026-03-08 18:00:13 +0900

    Title: 거울에 레이저 쏘기 2, Time: 349ms, Memory: 19MB, Status: Wrong Answer - Codetree

--- 260308/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
+++ 260308/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
@@ -11,38 +11,38 @@ public class Main {
     static int[] dy = {0, 1, 0, -1};
     public static void checkStart(int num) {
         if (1 <= num && num <= n) {           // 위
-            r = 0;
-            c = num -1;
+            r = -1;
+            c = num - 1;
             dirNum = 2;
         } 
         else if (n < num && num <= 2 * n) {   // 오른쪽
             r = num - n - 1;
-            c = n - 1;
+            c = n;
             dirNum = 3;
         } 
         else if (2 * n < num && num <= 3 * n) { // 아래
-            r = n - 1;
+            r = n;
             c = n - 1 - (num - 2 * n - 1);
             dirNum = 0;
         } 
         else {                                // 왼쪽
             r = n - 1 - (num - 3 * n - 1);
-            c = 0;
+            c = -1;
             dirNum = 1;
         }
     }
 
     public static int changeDir(char d, int dirNum) {
         if (d == 'r') { // -> \
-            if (dirNum == 0) dirNum = 1;
-            else if (dirNum == 3) dirNum = 2;
-            else if (dirNum == 2) dirNum = 3;
-            else dirNum = 0; //dirnum ==1 
-        } else { // -> /
             if (dirNum == 0) dirNum = 3;
             else if (dirNum == 1) dirNum = 2;
             else if (dirNum == 2) dirNum = 1;
-            else dirNum = 0; //dirnum ==3
+            else dirNum = 3;
+        } else { // -> /
+            if (dirNum == 0) dirNum = 1;
+            else if (dirNum == 1) dirNum = 0;
+            else if (dirNum == 2) dirNum = 3;
+            else dirNum = 2;
         }
         return dirNum;
     }
@@ -62,23 +62,26 @@ public class Main {
         // 시작좌표 확인\
         checkStart(startNum); // r,c 셋팅
         
-        int count = 1;
+        int count = 0;
         while(true) {
+             // 방향전환 + 이동
+            int nx = r + dx[dirNum];
+            int ny = c + dy[dirNum];
+            // 범위체크
+            if(nx < 0 || nx >= n || ny < 0 || ny >= n) break;
+            
+            r = nx;
+            c = ny;
             count++;
-
             // 거울 방향 체크
             if (grid[r][c] == '\\') {
                 dirNum = changeDir('r', dirNum);
             } else { // => /
                 dirNum = changeDir('l', dirNum);
             }
-            // 방향전환 + 이동
-            int nx = r + dx[dirNum];
-            int ny = c + dy[dirNum];
-            // 범위체크
-            if(nx < 0 || nx >= n || ny < 0 || ny >= n) break;
-            r = nx;
-            c = ny;
+          
+           
+            
         }
         System.out.print(count);
     }
