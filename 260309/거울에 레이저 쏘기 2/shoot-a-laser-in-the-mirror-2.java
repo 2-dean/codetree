fee0f52f532c449b85210e3943fb0cc1e74149af
2dean <104578954+2-dean@users.noreply.github.com>
2026-03-09 15:50:29 +0900

    Title: 거울에 레이저 쏘기 2, Time: 151ms, Memory: 13MB, Status: Passed - Codetree

--- 260309/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
+++ 260309/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.java
@@ -6,79 +6,76 @@ public class Main {
     
     public static int n;
     public static char[][] arr = new char[MAX_N][MAX_N];
-
+    
     public static int startNum;
-
-    public static int x = 0; // col
-    public static int y = 0; // row
-    public static int moveDir;
-                
-    // 시작위치와 방향 구하기
+    public static int x, y, moveDir;
+    
+    // 주어진 숫자에 따라
+    // 시작 위치와 방향을 구합니다.
     public static void initialize(int num) {
-        if (num <= n) { // 위
+        if(num <= n) {
             x = 0; y = num - 1; moveDir = 0;
-        } 
-        else if ( num <= 2 * n) {   // 오른쪽
+        }
+        else if(num <= 2 * n) {
             x = num - n - 1; y = n - 1; moveDir = 1;
-        } 
-        else if (num <= 3 * n) { // 아래
-            x = num - 1 ; y = n - (num - 2 * n); moveDir = 2;
-        } 
-        else {  // 왼쪽
+        }
+        else if(num <= 3 * n) {
+            x = n - 1; y = n - (num - 2 * n); moveDir = 2;
+        }
+        else {
             x = n - (num - 3 * n); y = 0; moveDir = 3;
         }
     }
-
+    
     public static boolean inRange(int x, int y) {
         return 0 <= x && x < n && 0 <= y && y < n;
     }
-
+    
+    // (x, y)에서 시작하여 nextDir 방향으로
+    // 이동한 이후의 위치를 구합니다.
     public static void move(int nextDir) {
-                    // 북 동 남 서
-        int[] dx = {1, 0, -1, 0};
-        int[] dy = {0, -1, 0, 1};
-
+        int[] dx = new int[]{1,  0, -1, 0};
+        int[] dy = new int[]{0, -1,  0, 1};
+        
         x += dx[nextDir];
         y += dy[nextDir];
         moveDir = nextDir;
     }
-
+    
     public static int simulate() {
         int moveNum = 0;
-        while (inRange(x, y)) {
-            // 0 <-> 1 , 2 <-> 3
-            if (arr[x][y] == '/') {
+        while(inRange(x, y)) {
+            // 0 <-> 1 / 2 <-> 3
+            if(arr[x][y] == '/')
                 move(moveDir ^ 1);
-            } // 0 <-> 3 / 1 <-> 2
-            else {
+            // 0 <-> 3 / 1 <-> 2
+            else
                 move(3 - moveDir);
-            }
+            
             moveNum += 1;
         }
+        
         return moveNum;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         // 입력
         n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            String s = sc.next();
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = s.charAt(j);
-            }
+        for(int i = 0; i < n; i++) {
+            String input = sc.next();
+            for(int j = 0; j < n; j++)
+                arr[i][j] = input.charAt(j);
         }
-
+        
         startNum = sc.nextInt();
-        // Please write your code here.
-        
-        // 시작 위치화 방향구하기
-        initialize(startNum); // r,c 셋팅
-        
+ 
+        // 시작 위치와 방향을 구합니다.
+        initialize(startNum);
         // (x, y)에서 moveDir 방향으로 시작하여
         // 시뮬레이션을 진행합니다.
         int moveNum = simulate();
-
+        
         System.out.print(moveNum);
     }
 }
