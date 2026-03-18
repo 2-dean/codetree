ad78a172b35c19346f2a7c7b63e6206e63cab668
2dean <104578954+2-dean@users.noreply.github.com>
2026-03-18 16:39:37 +0900

    Title: 삼각형 만들기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 260318/삼각형 만들기/create-triangle.java
+++ 260318/삼각형 만들기/create-triangle.java
@@ -4,23 +4,10 @@ public class Main {
     public static int [] y;
     public static int n;
 
-    static int maxX = Integer.MIN_VALUE;
-    static int minX = Integer.MAX_VALUE;
-    static int maxY = Integer.MIN_VALUE;
-    static int minY = Integer.MAX_VALUE;
-    
-    public static void getMaxTri (int i1, int i2, int i3) {    
-        for (int i = 0; i < n; i++) {
-            if (i == i1 || i == i2 || i == i3)
-                continue;
-            int cx = x[i], cy = y[i];
-            
-            maxX = Math.max(maxX, cx);
-            maxY = Math.max(maxY, cy);
 
-            minX = Math.min(minX, cx);
-            minY = Math.min(minY, cy);
-        } 
+     public static int area(int i, int j, int k) {
+        return Math.abs((x[i] * y[j] + x[j] * y[k] + x[k] * y[i]) - 
+                        (x[j] * y[i] + x[k] * y[j] + x[i] * y[k]));
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -36,15 +23,18 @@ public class Main {
         // Please write your code here.
 
 
-        
+        // 3개 점 선택
+        int maxArea = 0;
         for (int i = 0; i < n ; i++) {
             for (int j = i + 1; j < n ; j++) {
                 for(int k = j + 1; k < n; k++) {
-                    getMaxTri(i, j, k);
+                    // x값이 같은 쌍이 있고, y값도 같은 쌍이 있는경우에만 최대넓이 계산 
+                    if ((x[i] == x[j] || x[i] == x[k] || x[j] == x[k]) ||
+                        (y[i] == y[j] || y[i] == y[k] || y[j] == y[k]) 
+                    ) maxArea = Math.max(maxArea, area(i, j, k));
                 }
             }
         }
-        int ans = Math.abs(maxX - minX) * Math.abs(maxY - minY);
-        System.out.print((ans / 2 )* 2);
+        System.out.print();
     }
 }
