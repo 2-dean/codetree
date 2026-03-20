91a37ac2542bbce11cd3fd595f19ecb273e7ef0c
2dean <104578954+2-dean@users.noreply.github.com>
2026-03-20 14:34:17 +0900

    Title: 스승의 은혜 3, Time: 259ms, Memory: 13MB, Status: Wrong Answer - Codetree

---
+++ 260320/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int b = sc.nextInt(); // 예산
+        int[] p = new int[n];
+        int[] s = new int[n];
+        for (int i = 0; i < n; i++) {
+            p[i] = sc.nextInt(); // 선물
+            s[i] = sc.nextInt(); // 배송비
+        }
+        int max = 0;
+        // Please write your code here.
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            int students = 0;
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                p[i] /= 2; // 할쿠
+                if (b > sum) {
+                    sum += p[i];
+                    sum += s[i];
+                    students++;
+                }               
+            }
+            max = Math.max(max, students);
+        }
+        System.out.print(max);
+    }
+}
