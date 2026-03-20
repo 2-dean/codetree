0e507b4fce550159a5b5dbae0dbe23b76249dd22
2dean <104578954+2-dean@users.noreply.github.com>
2026-03-20 10:44:42 +0900

    Title: 이상한 폭탄 2, Time: 123ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260320/이상한 폭탄 2/strange-bomb-2.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 폭탄개수
+        int k = sc.nextInt(); // 거리k 이내면 같은번호의 폭탄 폭발
+        int[] bombs = new int[n];
+        for (int i = 0; i < n; i++) {
+            bombs[i] = sc.nextInt();
+        }
+        
+        int result = -1 ;
+        int maxBombNum = 0;
+        boolean isbombed = false;
+        int bombNum = 0;
+        if (n == k) {
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    if (bombs[i] == bombs[j]) {
+                        //isbombed = true;
+                        bombNum = bombs[i];
+                        //System.out.println("bombNum : " + bombNum);
+                        break;
+                    } 
+                }
+                maxBombNum = Math.max(maxBombNum, bombNum);
+
+            }
+        } else {
+            for (int i = 0 ; i < n - k; i++) {
+                for (int j = i + 1; j <= i + k; j++) {
+                    if (bombs[i] == bombs[j]) {
+                        //isbombed = true;
+                        bombNum = bombs[i]; 
+                        break;
+                    }
+                }
+                maxBombNum = Math.max(maxBombNum, bombNum);
+            }
+        }
+
+        result = maxBombNum;
+        System.out.print(maxBombNum);
+    }
+}
