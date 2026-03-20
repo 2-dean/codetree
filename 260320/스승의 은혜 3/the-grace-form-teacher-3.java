d8202ab7a6757ae4f642b8a39a074fbbd4c075d7
2dean <104578954+2-dean@users.noreply.github.com>
2026-03-20 15:10:40 +0900

    Title: 스승의 은혜 3, Time: 143ms, Memory: 10MB, Status: Passed - Codetree

--- 260320/스승의 은혜 3/the-grace-form-teacher-3.java
+++ 260320/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -14,24 +14,28 @@ public class Main {
             s[i] = sc.nextInt(); // 배송비
         }
         int max = 0;
+      
         // Please write your code here.
         for (int i = 0; i < n; i++) {
-            int sum = 0;
-            int students = 0;
+           
             int[] arr = new int[n];
             
             for (int j = 0; j < n; j++) {
-                if (i == j) continue;
-                p[i] /= 2; // 할쿠
-                // 새 배열에 담아보기
-                arr[i] = p[i] + s[i];
-                Arrays.sort(arr);
-
-                if (sum + arr[i] <= b) {
-                    sum += arr[i];
+                if (i == j) { // 한명씩 할쿠적용
+                    arr[j] = (p[j] / 2) + s[j]; // 할쿠
+                } else {
+                    arr[j] = p[j] + s[j];
+                }
+            }   
+            Arrays.sort(arr);
+            int sum = 0;
+            int students = 0;
+            
+            for (int k = 0; k < n; k++) {
+                if (sum + arr[k] <= b) {
+                    sum += arr[k];
                     students++;
                 }               
-                p[i] *= 2; //복구
             }
             
             max = Math.max(max, students);
