232812bbb1867e7dd92fc182b0be4dff04a0cb5d
2dean <104578954+2-dean@users.noreply.github.com>
2026-03-23 21:44:21 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 129ms, Memory: 11MB, Status: Passed - Codetree

--- 260323/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.java
+++ 260323/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.java
@@ -13,15 +13,31 @@ public class Main {
         }
 
 
-        int len = 1;
-        for (int i = 0 ; i < n; i++) {
-            for (int j = i + 1; j < n ; j++ ){
-                if (arr[i] == arr[j]) {
-                    len++;
-                }       
+        // 길이 1부터 n까지
+        for (int l = 1; l <= n; l++) {
+            boolean isDup = false;// 
+
+            // 길이가 l인 모든 부분 다 뽑아서 비교
+            for (int i = 0 ; i <= n - l ; i++) {
+                String sub1 = s.substring(i, i + l); 
+
+                for (int j = i + 1; j <= n - l ; j++ ){
+                    String sub2 = s.substring(j, j+l); // ㅓj부터 시작하는 l길이짜리
+
+                    if (sub1.equals(sub2)) {
+                        isDup = true;
+                        break;
+                    }      
+                }
+                if (isDup) break;
+            }
+            
+            if (!isDup) {
+               System.out.print(l);
+                return;
             }
         }
-        System.out.println(len);
+
 
     }
 }
