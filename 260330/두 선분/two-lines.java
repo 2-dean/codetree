1befc0ddde40cae867174509192cb231c5bb149c
2dean <104578954+2-dean@users.noreply.github.com>
2026-03-30 10:06:17 +0900

    Title: 두 선분, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260330/두 선분/two-lines.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int x3 = sc.nextInt();
+        int x4 = sc.nextInt();
+        // Please write your code here.
+        if (x1 == x3 
+         || x2 == x4
+         || x2 >= x3 
+         || x4 <= x1) {
+            System.out.print("intersecting");
+         } else{
+            System.out.print("nonintersecting");
+         }
+    }
+}
