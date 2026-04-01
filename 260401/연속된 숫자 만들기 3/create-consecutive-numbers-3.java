43610550b090d9a66b2e4cfb37b32e6d05ab045a
2dean <104578954+2-dean@users.noreply.github.com>
2026-04-01 14:04:28 +0900

    Title: 연속된 숫자 만들기 3, Time: 0ms, Memory: 0MB, Status: Compile Error - Codetree

--- 260401/연속된 숫자 만들기 3/create-consecutive-numbers-3.java
+++ 260401/연속된 숫자 만들기 3/create-consecutive-numbers-3.java
@@ -10,30 +10,12 @@ public class Main {
 
 
         int move = 0;
-        while (true) {
-            int diffLeft = b-a-1;
-            int diffRight = c-b-1;
 
-            // 이동공간 없음
-            if (diffLeft == 0 && difRight == 0) break;
+        int leftDiff = b-a-1;
+        int rightDiff = c-b-1;
 
-            if (diffLeft == 0 && diffRight > 0) { //앞은 공간 없고 오른쪽 공간있을때 왼쪽끝 -> 중간
-                a = c-b;
-                //a->b, b- >a
-                int temp = a
-                a = b;
-                b =
-                move++;
-            } 
-            if (diffRight == 0 && diffLeft > 0) { // 오른쪽 공간ㅇ 없고 왼쪽 이동가능할때
-                c = b+1;
-                move++;
-            } 
-            if () { // 바로 앞/뒤 인경우 중간으로
-                
-            } else if (diffRight == 0) {
+        M
+        System.out.print(leftDiff +rightDiff);
 
-            }
-        }
     }
 }
