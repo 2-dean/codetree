adc88a3bac6a5b4f1406a66460c652b1960ec0d6
2dean <104578954+2-dean@users.noreply.github.com>
2026-04-21 13:06:11 +0900

    Title: 기울어진 직사각형, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260421/기울어진 직사각형/slanted-rectangle.java
+++ 260421/기울어진 직사각형/slanted-rectangle.java
@@ -1,54 +1,54 @@
 import java.util.Scanner;
 public class Main {
+    static int n;
+    static int[][] grid;
+
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n;
+    }
+
+    static int getScore (int x, int y, int w, int h) {
+        int[] dx = {-1, -1, 1, 1};
+        int[] dy = {1, -1, -1, 1};
+        int[] moveNum = new int[]{w, h, w, h};
+
+        int sum = 0;
+        for (int d = 0; d < 4; d++) {
+            for (int q = 0; q < moveNum[d]; q++) {
+                x += dx[d]; 
+                y += dy[d];
+
+                // 
+                if (!inRange(x, y))
+                    return 0;
+                sum += grid[x][y];
+            }
+        }
+        return sum;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[][] grid = new int[n][n];
+        n = sc.nextInt();
+        grid = new int[n][n];
+        
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 grid[i][j] = sc.nextInt();
-        // Please write your code here.
-        int maxSum = 0;
-        // 1. 시작점 정하기 (아래쪽 꼭짓점)
+        
+        int answer = 0;
+        //(i,j)에서 시작 해서 1,2,3,4 방향 탐색
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-
-                //가능한 모든 가로 세로 조합 탐색
+            for(int j = 0; j < n;  j++) {
+                //순서대로 길이 w, h, w, h 만큼 이동하면서 그려지는 기울어진 직사각형을 잡아봄
+            
                 for (int w = 1; w < n; w++) {
                     for (int h = 1; h < n; h++) {
-                        
-                        int currentSum = 0;
-                        int x = i;
-                        int y = j;
-                        boolean isPossible = true;
-
-                        // 방향순서 : 오위 
-                        int[] dx = {-1, -1, 1, 1};
-                        int[] dy = {1, -1, -1, 1};
-                        int[] moveSteps ={w, h, w, h};
-
-                        for (int d = 0; d < 4; d++) {
-                            for (int step = 0; step < moveSteps[d]; step++) {
-                                x += dx[d];
-                                y += dy[d];
-
-                                // 격자 밖으로 나가면 탈락
-                                if (x < 0 || x >= n || y < 0 || y >= n) {
-                                    isPossible = false;
-                                    break;
-                                }
-                                currentSum += grid[x][y];
-                            }
-                            if (!isPossible) break;
-                        }
-                        // 네모 그리는데 성공했으면 최댓값 갱신
-                        if (isPossible) {
-                            maxSum = Math.max(maxSum, currentSum);
-                        }
+                        answer = Math.max(answer, getScore(i, j, w, h));
                     }
                 }
             }
         }
-        System.out.print(maxSum);
+
+        System.out.print(answer);
     }
 }
